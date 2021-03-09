74e3fcd24feebd2023527c900a349fba0fcd5d82
Ash Rahman <ashiq.r31@gmail.com>
2021-03-09 09:04:27 -0500

    swap file contents

--- challenges/unit_testing/tests/playlist_functions_test.py
+++ challenges/unit_testing/tests/playlist_functions_test.py
@@ -20,31 +20,13 @@ def playlist():
 
 # Question 1: Complete the test for get_playlist_titles function
 def test_get_titles(playlist):
-	assert get_playlist_titles(playlist)
+	expected = ["Sometimes", "Mothership Connection (Star Child)", "Another Brick in the Wall", 
+	"Unfunky UFO", "Mississippi Goddamn", "Four Women", "Killing Me Softly", "Killing Me Softly"]
+	actual = get_playlist_titles(playlist)
+	assert actual == expected
 
 # Question 2: Write two test functions for search_by_artist
-def test_search_pink(playlist):
-	expected = ["Another Brick in the Wall"]
-	actual = search_by_artist(playlist, "Pink Floyd")
-	assert actual == expected
 
-def test_search_nina(playlist):
-	expected = ["Mississippi Goddamn", "Four Women"]
-	actual = search_by_artist(playlist, "Nina Simone")
-	assert actual == expected
 
 # Question 3: Write two test functions for search_by_title
-def test_search_sometimes(playlist):
-	expected = [
-		{'artist': 'Bilal', 'title': 'Sometimes'},
-	]
-	actual = search_by_title(playlist, "Sometimes")
-	assert actual == expected
 
-def test_search_killing_me(playlist):
-	expected = [
-		{'artist': 'Roberta Flack', 'title': 'Killing Me Softly'},
-		{'artist': 'Fugees', 'title': 'Killing Me Softly'}
-	]
-	actual = search_by_title(playlist, "Killing Me Softly")
-	assert actual == expected
