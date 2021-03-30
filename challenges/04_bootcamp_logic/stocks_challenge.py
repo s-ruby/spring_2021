02814edf1d523a9bacc58403bd434db59b492e08
Serena Killion <37127943+s-ruby@users.noreply.github.com>
2021-03-30 13:59:00 -0400

    Shorten Challenge

--- challenges/04_bootcamp_logic/stocks_challenge.py
+++ challenges/04_bootcamp_logic/stocks_challenge.py
@@ -5,16 +5,14 @@ print()
 # Creating variables to store the current (approximate) market price of these 5 companies - Amazon, Apple, Facebook, Google and Microsoft.
 
 amazon = 3000
-apple = 100
 fb = 250
 google = 1400
-msft = 200
 
 print("Challenge 3.2.1: Taking user input")
 # TODO: Write code to ask the client his name and save it to a variable.
 # TODO: Write code to ask the client his savings and save it to another variable.
 # TODO: Write code to ask the client the stock he is interested in and save it to another variable, as shown below.
-stock = input("Which stock are you interested in? Type 'amzn' for Amazon, 'aapl' for Apple, 'fb' for Facebook, 'goog' for Google and 'msft' for Microsoft.")
+stock = input("Which stock are you interested in? Type 'amzn' for Amazon, 'fb' for Facebook, 'goog' for Google.")
 print()
 
 print("Challenge 3.2.2: Perform user-specific calculations")
@@ -32,9 +30,9 @@ else ...
 print()
 
 print("Challenge 3.2.3: Output for the user the result")
-# TODO: COnce you have calculated the number of stocks that can be purchased, print the result for the client. Result should be in a format like this:
+# TODO: Once you have calculated the number of stocks that can be purchased, print the result for the client. Result should be in a format like this:
 
-# Alex has $5000 in savings and he can buy 50 shares of Apple at the current price of $100.
+# Alex has $1400 in savings and he can buy 1 shares of Google at the current price of $1400.
 
 print()
 
